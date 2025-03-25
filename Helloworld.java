36e35b8f3abf6465eee32e2572a947b8b3135f4a
CalebStore <storey.c133738@stu.sanjac.edu>
2025-03-24 22:27:12 -0500

    Add files via upload

--- Helloworld.java
+++ Helloworld.java
@@ -1,5 +1,20 @@
 class Helloworld{
     public static void main(String []args){
+        //Entry point
         System.out.println("Hello, World!");
+        //Prints Hello, World!
+        System.out.println("Caleb Storey");
+        //Print Name
+        System.out.println("BBQ");
+        //Print Favorite Food
+        System.out.println("Robin ;Williams");
+        //Print Celebrity I'd like to meet
+        System.out.println("Washing Dishes");
+        //Print Least Favorite Chore
+        System.out.println("Playing VR");
+        //Print It is what I would be doing 
     }
+    //main() is used as the entrypoint to any Java program 
+    // System.out.println() is used to display a desired output of a program, sysetem being class, out a field, println() prints a string.
+    //Comments explain any bugs or errors found and replaced. Additonally used for any documentation of code changed or in need of changing.
 }
